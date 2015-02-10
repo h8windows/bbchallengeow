c7e9f6cf99a1e6281547fe90e75abb54c06c9dbc
Chris Holmes <h8windows@gmail.com>
2015-02-10 08:44:54 -0800

    added .travis.yml file moved test method

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
-group :test do
-  gem 'pullreview-coverage', require: false
-end
+require 'pullreview/coverage_reporter'
+PullReview::CoverageReporter.start
+
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -14,4 +15,8 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  group :test do
+    gem 'pullreview-coverage', require: false
+  end
+  
 end
